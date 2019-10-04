75954af7bd4b3569050128030bf2f131a5986ef8
Sean Little <slittle@mymail.mines.edu>
2019-10-04 15:33:41 -0600

    Working voting system and basic view templates

--- csmsaleh/evaluation/urls.py
+++ csmsaleh/evaluation/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 
 from . import views
 
+app_name = 'evaluation'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
+    # ex: /evaluation/5/
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    # ex: /evaluation/5/results/
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    # ex: /evaluation/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
