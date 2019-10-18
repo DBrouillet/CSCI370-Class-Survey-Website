ddf2d94bccc2a098b62273068c3e6837edeaf04a
unknown <visitor0827@gmail.com>
2019-10-18 11:12:11 -0600

    modified login content

--- csmsaleh/evaluation/admin.py
+++ csmsaleh/evaluation/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth import login
 
 from .models import Question, Choice
 
@@ -16,6 +17,7 @@ class QuestionAdmin(admin.ModelAdmin):
     list_display = ('question_text', 'pub_date', 'was_published_recently')
     list_filter = ['pub_date']
     search_fields = ['question_text']
+
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
