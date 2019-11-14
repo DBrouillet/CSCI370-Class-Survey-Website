5d53f3e0274d525cc0b448aaf1b2c2f678a6af20
Sean Little <slittle@mymail.mines.edu>
2019-11-13 23:10:08 -0700

    Even more settings changes for whitenoise

--- csmsaleh/settings.py
+++ csmsaleh/settings.py
@@ -48,11 +48,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
-MIDDLEWARE_CLASSES = (
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-)
 
 ROOT_URLCONF = 'csmsaleh.urls'
 
