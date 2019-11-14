12c1bd31d952e00a9619283a6699b308d72c7e8b
Sean Little <slittle@mymail.mines.edu>
2019-11-13 22:11:53 -0700

    Fixed an error in settings.py file

--- csmsaleh/settings.py
+++ csmsaleh/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = 'static'
 
 LOGIN_REDIRECT_URL = 'base_site'
 LOGOUT_REDIRECT_URL = 'base_site' # add login and logon functionality, replace it with home page
