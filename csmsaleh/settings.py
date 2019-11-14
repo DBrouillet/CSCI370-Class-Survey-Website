274442313857e6a29d943fda69790283dc77779d
Sean Little <slittle@mymail.mines.edu>
2019-11-13 22:33:21 -0700

    Updated settings for heroku

--- csmsaleh/settings.py
+++ csmsaleh/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^01n77__vhqei)t^*h!+t(sp7+)3i-()39z+1jm-mw834390@1'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [csmevaluator.herokuapp.com']
 
 
 # Application definition
